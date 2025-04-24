d8dc96f8235ed6f7ed6674640deb13f9f3fc4855
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2025-04-24 15:10:32 +0200

    fix docker nftables/iptables problem

--- wsl_addons.sh
+++ wsl_addons.sh
@@ -6,8 +6,16 @@ symlink_xdg_open() {
   sudo ln -s /mnt/c/Windows/explorer.exe /usr/local/bin/xdg-open
 }
 
+fix_docker() {
+  # docker does not work with nftables
+  # https://forums.docker.com/t/failing-to-start-dockerd-failed-to-create-nat-chain-docker/78269
+  sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+  sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+}
+
 main() {
   symlink_xdg_open
+  fix_docker
 }
 
 main
